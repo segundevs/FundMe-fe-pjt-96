8c6d10f7197130cd7dac41f1d56edb43e1e7258e
segundevs <tushbash@gmail.com>
2021-06-28 05:22:40 -0700

    added slideshow

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
 //Slide show
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll('.dot');
+
+let currentSlide = 0;
+
+const slider = (n) => {
+  slides.forEach((slide) => {
+    slide.style.display = "none"
+    dots.forEach((dot) => {
+      dot.classList.remove("active")
+    })
+  })
+  slides[n].style.display = "block"
+  dots[n].classList.add("active")
+}
+document.addEventListener("DOMContentLoaded", slider(currentSlide));
+
+const next = () => {
+  currentSlide >= slides.length - 1 ? currentSlide = 0 : currentSlide++
+  slider(currentSlide);
+}
+
+const prev = () => {
+  currentSlide <= 0 ? currentSlide = slides.length - 1 : currentSlide--
+  slider(currentSlide);
+}
+
+document.querySelector(".next").addEventListener('click', next);
+document.querySelector(".prev").addEventListener('click', prev);
+
+
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    slider(index)
+    currentSlide = i
+  })
+});
 
 //Payment gateway
